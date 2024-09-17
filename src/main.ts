0789465c380b3315583058d93b5540d47d947acf
Guilherme <guilhermemrr@gmail.com>
2024-09-17 16:03:22 +0200

    updated version. added skeleton for standalone component demo

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,33 @@
-import { AdyenCheckout, Dropin } from '@adyen/adyen-web/auto';
-import '@adyen/adyen-web/styles/adyen.css';
 import createSession from './services/create-session';
 import { DEFAULT_COUNTRY, DEFAULT_LOCALE } from './utils/constants';
-import './style.css';
+
+import { AdyenCheckout, Dropin } from '@adyen/adyen-web/auto';
+import '@adyen/adyen-web/styles/adyen.css';
 
 async function createAdyenCheckout() {
-  const session = await createSession({
-    countryCode: DEFAULT_COUNTRY,
-    shopperLocale: DEFAULT_LOCALE,
-  });
+  const session = await createSession({ countryCode: DEFAULT_COUNTRY, shopperLocale: DEFAULT_LOCALE });
 
   const checkout = await AdyenCheckout({
     session: {
       id: session.id,
       sessionData: session.sessionData,
     },
-    countryCode: DEFAULT_COUNTRY,
     environment: 'test',
     clientKey: import.meta.env.VITE_ADYEN_CLIENT_KEY,
+    countryCode: DEFAULT_COUNTRY,
     locale: DEFAULT_LOCALE,
-    amount: session.amount,
+    onPaymentCompleted() {
+      console.log('Payment completed');
+    },
+    onPaymentFailed() {
+      console.log('Payment failed');
+    },
     onError(error) {
-      console.error(error);
-    },
-    onPaymentCompleted(data, component) {
-      console.log(data, component);
-    },
-    onPaymentFailed(data, component) {
-      console.log(data, component);
+      console.log('onError', error);
     },
   });
 
   const dropin = new Dropin(checkout);
-
   dropin.mount('#adyen-checkout');
 }
 
